5a4c003241f3cf6fa017ff3adf4a4edc864be894
samridh11-11 <samridh.suresh.2005@gmail.com>
2025-06-12 12:08:28 +0530

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
+
 const {Command} = require('commander');
+const {PrismaClient}=require('@prisma/client');
+
 const program = new Command();
+const prisma = new PrismaClient();
+
 
 program
 .name(' Code Snippet CLI')
 .description ('My Code Snippet CLI program')
 .version('1.0.0');
-
-
 
 program
 .command('hi')
@@ -16,5 +19,7 @@ program
 
 });
 
-
-program.parse(process.argv);
+program
+.command('add <name>')
+.description('lets you add the code snippet');
+.requiredOption ('-l,--language<lang>','specify the programming language')
