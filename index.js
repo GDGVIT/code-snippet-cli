67fe46d91887cc7299a9c74b302e004000c0716e
Aditya Chowdhary <adityachowdhary04@gmail.com>
2025-06-18 05:13:01 +0530

    fix: cleaned up repo

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 #!/usr/bin/env node
 
-const {Command} = require('commander');
-const {PrismaClient}=require('@prisma/client');
+import { Command } from 'commander';
+// const {PrismaClient}=require('@prisma/client');
 
 const program = new Command();
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 
 program
 .name(' Code Snippet CLI')
-.description ('My Code Snippet CLI program')
+.description('My Code Snippet CLI program')
 .version('1.0.0');
 
 program
-.command('hi')
+.command('hi <name>')
 .description('Says hi to the user')
-.action(() =>{ console.log ('Hello User, Welcome to the CLI');
+.option('-f')
+.action(() =>{ console.log('Hello User, Welcome to the CLI');});
 
-});
-
-program
-.command('add <name>')
-.description('lets you add the code snippet');
-.requiredOption ('-l,--language<lang>','specify the programming language')
+program.parse();
